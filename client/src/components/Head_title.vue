e4139ed6319e2035deea4e0bbf91334b878484d2
tarik_kabaki <herogame1970@gmail.com>
2025-02-28 01:36:47 +0000

    chore: adjusting API endpoints & cleaning code

--- client/src/components/Head_title.vue
+++ client/src/components/Head_title.vue
@@ -2,8 +2,8 @@
 </script>
 <template>
         <div class="w-full h-[150px] flex justify-center items-center">
-        <div class="flex flex-col gap-4 text-center font-semibold tracking-wide">
-        <h1 class="text-5xl text-slate-300">Todo App</h1>
+        <div class="flex flex-col gap-2 text-center font-semibold tracking-wide">
+        <h1 class="text-5xl text-slate-300 font-bold">Todo List</h1>
         <h2 class="text-[14px] text-gray-500">This is your Task manager, stay focused, stay clean</h2>
         </div>
     </div>
